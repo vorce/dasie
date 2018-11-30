1aaa079717626a85a5b21c49d0de667a062bae85
Joel Carlbark <joel.carlbark@meltwater.com>
2018-11-30 14:56:52 +0100

    Flesh out insert

--- lib/cuckoo_filter.ex
+++ lib/cuckoo_filter.ex
@@ -8,16 +8,82 @@ defmodule Dasie.CuckooFilter do
   - https://www.cs.cmu.edu/%7Edga/papers/cuckoo-conext2014.pdf
   - https://github.com/bdupras/guava-probably/blob/master/src/main/java/com/duprasville/guava/probably/CuckooFilter.java
   """
+  use Bitwise
 
-  @default_fingerprint_length 7
+  defmodule Bucket do
+    @moduledoc """
+    A bucket can have multiple entries
+    """
+    defstruct id: nil,
+              entries: MapSet.new()
+  end
 
-  defstruct buckets: %{}
+  @default_fingerprint_length 16
+  @max_displacements 500 # number of relocations allowed before giving up on an item. If we reach this this hash table is considered too full.
 
-  def insert(%__MODULE__{}, item) when is_binary(item) do
-    hash = :erlang.md5(item)
-    # fingerprint = fingerprint(hash)
-    # bucket_1 = hash(item)
-    # bucket_2 = bucket_1 xor hash(fingerprint)
+  # A basic cuckoo hash table consists of an array of buckets
+  defstruct buckets: %{},
+            load: 0,
+            fingerprint_size: @default_fingerprint_length,
+            bucket_size: 4,
+            bucket_count: 10,
+            max_keys: 100_000
+
+  def new(opts \\ []) do
+    max_keys = opts[:max_keys] || 100_000
+
+    cuckoo = %__MODULE__{max_keys: max_keys}
+    keys_per_bucket_size = cuckoo.max_keys / cuckoo.bucket_size
+    log2 = :math.log(keys_per_bucket_size) / :math.log(2)
+    bucket_count = :math.pow(2, Float.ceil(log2)) |> trunc()
+    %__MODULE__{cuckoo | bucket_count: bucket_count}
+  end
+
+  def insert(%__MODULE__{} = cuckoo, item), do: insert(cuckoo, item, 0)
+
+  defp insert(%__MODULE__{}, _item, relocation_round) when relocation_round >= @max_displacements, do: {:error, :full}
+  defp insert(%__MODULE__{} = cuckoo, item, relocation_round) do
+    # each item x has two candidate buckets determined by hash functions h1(x) and h2(x)
+    fingerprint = fingerprint(item, cuckoo.fingerprint_size) |> IO.inspect(label: "fingerprint")
+    bucket_1 = item |> hash() |> rem(cuckoo.bucket_count) |> IO.inspect(label: "bucket1")
+    bucket_2 = bucket_1 |> bxor(hash(fingerprint)) |> rem(cuckoo.bucket_count) |> IO.inspect(label: "bucket2") # = bucket_1 xor hash(fingerprint)
+
+    cond do
+      has_space?(cuckoo, bucket_1) ->
+        add_entry(cuckoo, bucket_1, fingerprint)
+      has_space?(cuckoo, bucket_2) ->
+        add_entry(cuckoo, bucket_2, fingerprint)
+      true ->
+        IO.puts("No space in either bucket #{inspect([bucket_1, bucket_2])}, relocating...")
+        cuckoo
+        |> relocate(bucket_1, bucket_2)
+        |> insert(item, relocation_round + 1)
+    end
+  end
+
+  # def lookup() do
+  # end
+
+  # def delete() do
+  # end
+
+  defp hash(item) do
+    :erlang.phash2(item)
+  end
+
+  defp has_space?(%__MODULE__{} = cuckoo, bucket_id) do
+    bucket = Map.get(cuckoo.buckets, bucket_id, %Bucket{id: bucket_id})
+    (MapSet.size(bucket.entries) < cuckoo.bucket_size) |> IO.inspect(label: "#{bucket_id} has space")
+  end
+
+  defp add_entry(%__MODULE__{} = cuckoo, bucket_id, fingerprint) do
+    bucket = Map.get(cuckoo.buckets, bucket_id, %Bucket{id: bucket_id})
+    new_bucket = %Bucket{bucket | entries: MapSet.put(bucket.entries, fingerprint)}
+    %__MODULE__{cuckoo | buckets: Map.put(cuckoo.buckets, bucket_id, new_bucket), load: cuckoo.load + 1}
+  end
+
+  defp relocate(%__MODULE__{} = cuckoo, bucket_1, bucket_2) do
+    cuckoo
   end
 
   @doc """
@@ -26,7 +92,7 @@ defmodule Dasie.CuckooFilter do
   ...
   > Once the fingerprint was 7 bits long, the load factor of the cuckoo filter mirrored that of a cuckoo hash table that used two perfectly random hash functions.
   """
-  def fingerprint(hash, size \\ @default_fingerprint_length) do
-
+  def fingerprint(item, size \\ @default_fingerprint_length) do
+    :erlang.phash2(item) &&& (1 <<< size) - 1
   end
 end
--- test/cuckoo_filter_test.exs
+++ test/cuckoo_filter_test.exs
@@ -3,9 +3,24 @@ defmodule Dasie.CuckooFilterTest do
 
   alias Dasie.CuckooFilter
 
+  describe "new/1" do
+    test "creates a new default struct" do
+      assert %CuckooFilter{} = CuckooFilter.new()
+    end
+
+    test "creates a new struct with specified max_keys" do
+      max_keys = 100
+      assert %CuckooFilter{max_keys: max_keys} = CuckooFilter.new(max_keys: max_keys)
+    end
+  end
+
   describe "insert/2" do
     test "can insert single entry" do
+      result = CuckooFilter.insert(CuckooFilter.new(), "hello")
+      assert Map.size(result.buckets) == 1
 
+      bucket = result.buckets |> Map.values() |> List.first()
+      assert MapSet.size(bucket.entries) == 1
     end
   end
 end
